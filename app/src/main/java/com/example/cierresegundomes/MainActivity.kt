461b3d0433426cc04239572822c9768d950c2939
Reinier de Armas <reinier.dearmas@gmail.com>
2024-04-21 20:28:11 -0400

    first

---
+++ app/src/main/java/com/example/cierresegundomes/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.cierresegundomes
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.cierresegundomes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        //setContentView(R.layout.activity_main)
+        }
+    }
